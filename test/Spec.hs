dafd8187a63cd968f0f6a116a1b75e64470495a6
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-04-17 21:07:53 +0200

    Add doctest to tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+module Main where
+
+-- * Doctests
+-- * Test utils
+-- * Implement tests from spec
+
+import Test.DocTest (doctest)
+import Test.Hspec
+import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "DocTests:"
+  doctest ["app", "src"]
